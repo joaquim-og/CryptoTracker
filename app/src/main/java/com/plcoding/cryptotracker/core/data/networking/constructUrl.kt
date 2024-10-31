473d8eacf4eef0522a62403f1c33bf5f0ad85195
Joaquim Gomes <joaquim_og@hotmail.com>
2024-10-31 08:35:22 -0300

    Refactor: Move utility classes to core package
    This commit restructures the project by moving utility classes and functions, such as `Error`, `Result`, and `CoinCodeToResource`, to a new core package.
    
    Specifically, these classes are relocated to the `core.domain.util` and `core.presentation.util` sub-packages within the core package.
    
    Additionally, a new `NetworkError` enum class is introduced in the `core.domain.util` package to represent different types of network errors.
    
    This refactoring improves the organization of the project and promotes code reuse.

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/constructUrl.kt
@@ -0,0 +1,7 @@
+package com.plcoding.cryptotracker.core.data.networking
+
+//fun constructUrl(url: String): String {
+//    return when {
+//        url.contains("?") -> "$url&vs_currency=usd"
+//    }
+//}
