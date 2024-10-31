41cf270d6593ba0fc070dfbe2bcb8ba3bf2397b4
Joaquim Gomes <joaquim_og@hotmail.com>
2024-10-31 11:14:58 -0300

    feat: initialize app structure
    This commit sets up the initial application structure including:
    
    - Creates `CryptoTrackerApp` class and registers it in `AndroidManifest.xml`.
    - Adds internet permission to `AndroidManifest.xml`.
    - Sets up Koin dependency injection with `appModule`.
    - Creates `CoinListScreen` and integrates `CoinListViewModel` using Koin.
    - Adds network call functionality and related logic.
    - Creates `RemoteCoinDataSource` and related logic.

---
+++ app/src/main/java/com/confradesTecch/cryptotracker/CryptoTrackerApp.kt
@@ -0,0 +1,19 @@
+package com.confradesTecch.cryptotracker
+
+import android.app.Application
+import com.confradesTecch.cryptotracker.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class CryptoTrackerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@CryptoTrackerApp)
+            androidLogger()
+            modules(appModule)
+        }
+    }
+}
